94317ed6576c88140c46f65c3c7d3c523421a432
Qosimjondev <sobirov_qosimjon@mail.ru>
2021-11-25 09:30:48 +0500

    mainga breadcrumb qo'shildi

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -4,32 +4,22 @@
 /* @var $form yii\bootstrap4\ActiveForm */
 /* @var $model \frontend\models\ContactForm */
 
+use yii\bootstrap4\Breadcrumbs;
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
 use yii\captcha\Captcha;
 
-$this->title = 'Contact';
+$this->title = 'Contact Us';
+//$this->params['breadcrumbs'][] = ['label' => 'Login','url'=>['site/login']];
 $this->params['breadcrumbs'][] = $this->title;
+
+
 ?>
 
 
 
 	<!-- Inner Page Breadcrumb -->
-	<section class="inner_page_breadcrumb">
-		<div class="container">
-			<div class="row">
-				<div class="col-xl-6 offset-xl-3 text-center">
-					<div class="breadcrumb_content">
-						<h4 class="breadcrumb_title">Contact Us</h4>
-						<ol class="breadcrumb">
-						    <li class="breadcrumb-item"><a href="#">Home</a></li>
-						    <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
-						</ol>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+
 
 	<!-- How It's Work -->
 	<section class="our-contact">
